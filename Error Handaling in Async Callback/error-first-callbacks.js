b90aea6a5980e221bfd174a1682ebf92ba366eda
Rama krishna Raju Rudraraju <rrkr2016@uab.edu>
2021-01-30 21:28:06 -0600

    Error First Callback Functions Example

---
+++ Error Handaling in Async Callback/error-first-callbacks.js
@@ -0,0 +1,27 @@
+// Example of error-first callbacks
+function calculateSquare(number, callback) {
+    setTimeout(function() {
+        if (typeof number !== 'number') {
+            callback(new Error('Argument of type number is expected'));
+            return;
+        }
+        const result = number * number;
+        callback(null, result);
+    }, 1000);
+}
+
+calculateSquare('bad argument', function (error, result) {
+    if (error !== null) {
+        console.log('Caught error: ' + String(error));
+        return;
+    }
+    console.log(result);
+});
+
+calculateSquare(10, function (error, result) {
+    if (error !== null) {
+        console.log('Caught error: ' + String(error));
+        return;
+    }
+    console.log(result);
+});
